5b7236dafb84ef97789241574a7036ce059814ee
Christy Liong <stliong@o365.fcu.edu.tw>
2019-01-24 17:40:28 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,3 +1,18 @@
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  This script is to perform the micro-expression recognition using STSTNet with LOSOCV protocol.
+%  Reference:
+%  xxxxxxxx
+%
+%  The files include:
+%  1) main.m : Script which trains and tests the STSTNet 
+%  2) STSTNet.mat : The STSTNet architecture design
+%  3) video442subName.txt : List of subject's name  
+%  4) input : Input data (28x28x3) arranged in LOSOCV manner
+%
+%  Matlab version was written by Sze Teng Liong and was tested on Matlab 2018b
+%  If you have any problem, please feel free to contact Sze Teng Liong (stliong@fcu.edu.tw)
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 fid = fopen('video442subName.txt');
 garbage = textscan(fid,'%s','delimiter','\n');
 subName =garbage{1}; 
